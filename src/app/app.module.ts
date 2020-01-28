9d06cbf720307091b2d333e21f4460f0b30ba286
Nitai Charan <nitaicharan@gmail.com>
2020-01-28 13:12:11 -0300

    36. Criando as Primeiras Rotas
    - routerLink
    - routerLinkActive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routers';
 
 
@@ -16,12 +17,12 @@ import { ROUTES } from './app.routers';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
--- src/app/app.routers.ts
+++ src/app/app.routers.ts
@@ -1,6 +1,8 @@
-import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
 ];
