76a99c045e6360cb4b04eefa4d48327868d9825c
Nitai Charan <nitaicharan@gmail.com>
2020-01-29 21:12:22 -0300

    44. Tratamento de Erros com o Operador Catch
    - catch

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 import { Restaurant } from "./restaurant/restaurant.model";
 import { MEAT_API } from "app/app.api";
+import { ErrorHandler } from "app/app.error-handler";
 @Injectable()
 export class RestaurantsService {
   private rests: Restaurant[];
 
   constructor(private http: Http) { }
 
-  getRestaurants = (): Observable<Restaurant[]> => this.http.get(`${MEAT_API}`).map(response => response.json().restaurants);
+  getRestaurants = (): Observable<Restaurant[]> =>
+    this.http
+      .get(`${MEAT_API}`)
+      .map(response => response.json().restaurants)
+      .catch(ErrorHandler.handleError);
 }
