246124b31f66705a1c96e11245d85f05c44a660e
Nitai Charan <nitaicharan@gmail.com>
2020-02-03 16:47:23 -0300

    74. Criando Validadores Personalizados
    - AbstractControl

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -35,6 +35,14 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', [Validators.required])
+    }, {
+      validator: (group: AbstractControl): { [key: string]: boolean } => {
+        const email = group.get('email')
+        const emailConfirmation = group.get('emailConfirmation')
+        if (!email || !emailConfirmation) return undefined
+        if (email.value !== emailConfirmation.value) return { emailsNotMatch: true }
+        return undefined
+      }
     })
   }
 
