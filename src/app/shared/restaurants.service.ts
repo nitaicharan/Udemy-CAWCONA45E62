3c28d5ff667a2d0b75d9d5c353f349ae1d5aa17e
Nitai Charan <nitaicharan@gmail.com>
2020-02-12 14:15:13 -0300

    105. Operadores debounceTime e distinctUntilChanged

--- src/app/shared/restaurants.service.ts
+++ src/app/shared/restaurants.service.ts
@@ -14,24 +14,24 @@ export class RestaurantsService {
 
   constructor(private http: Http) { }
 
-  getRestaurants = (): Observable<Restaurant[]> =>
-    this.http
-      .get(`${MEAT_API}/restaurants`)
+  getRestaurants(search?: string): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}restaurants`, { params: { q: search } })
       .map(response => response.json())
-      .catch(ErrorHandler.handleError);
+      .catch(ErrorHandler.handleError)
+  }
 
   getRestaurant = (id: string): Observable<Restaurant> =>
-    this.http.get(`${MEAT_API}/restaurants/${id}`)
+    this.http.get(`${MEAT_API}restaurants/${id}`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
 
   getReviews = (id: string): Observable<any> =>
-    this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+    this.http.get(`${MEAT_API}restaurants/${id}/reviews`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
 
   getMenu = (id: string): Observable<MenuItem[]> =>
-    this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+    this.http.get(`${MEAT_API}restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
 }
