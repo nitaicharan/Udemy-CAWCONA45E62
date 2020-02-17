12fe81f40a7d7a5d9274b0456dd414e199041880
Nitai Charan <nitaicharan@gmail.com>
2020-02-17 18:20:53 -0300

    127. Utilizando Zonas

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,29 +1,35 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { NotificationService } from './shared/messages/notification.service';
 import { LoginService } from './security/login/login.service';
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
-  constructor(private notificationService: NotificationService, private injector: Injector) {
+  constructor(
+    private notificationService: NotificationService,
+    private injector: Injector,
+    private zone: NgZone
+  ) {
     super();
   }
 
   handleError(errorResponse: HttpErrorResponse | any) {
     if (errorResponse instanceof HttpErrorResponse) {
       let message: string = errorResponse.error.message;
-      switch (errorResponse.status) {
-        case 401:
-          this.injector.get(LoginService).handleLogin();
-          break;
-        case 403:
-          message = message ? message : 'Não autorizado';
-          break;
-        case 404:
-          message = message ? message : 'Recurso não encontrado. Verifique o console para mais detalhes!';
-          break;
-      }
-      this.notificationService.notify(message)
+      this.zone.run(() => {
+        switch (errorResponse.status) {
+          case 401:
+            this.injector.get(LoginService).handleLogin();
+            break;
+          case 403:
+            message = message ? message : 'Não autorizado';
+            break;
+          case 404:
+            message = message ? message : 'Recurso não encontrado. Verifique o console para mais detalhes!';
+            break;
+        }
+        this.notificationService.notify(message)
+      })
     }
     super.handleError(errorResponse)
   }
